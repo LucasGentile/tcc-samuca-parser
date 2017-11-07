b4129e013b223f7b327ef84d8d8e7e7d240eabde
Lucas_Gentile <lucasgpc@gmail.com>
2017-11-07 15:32:57 -0200

    Primeira versão pronta do Portfolio 50304030

--- src/main/java/ProcessTickers.java
+++ src/main/java/ProcessTickers.java
@@ -1,5 +1,6 @@
 import dto.TickerInfoDTO;
 import enums.PortfolioGroupNamesEnum;
+import enums.SortingTypeEnum;
 import excel.ExcelReader;
 import excel.ExcelWriter;
 
@@ -8,6 +9,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import static enums.PortfolioGroupNamesEnum.*;
+import static enums.SortingTypeEnum.BMME;
+import static enums.SortingTypeEnum.INV;
+import static enums.SortingTypeEnum.OP;
 
 /**
  * Created by lucas on 05/11/2017.
@@ -19,18 +23,18 @@ public class ProcessTickers {
 //                                                "2015",
 //                                                "2014",
 //                                                "2013",
-                                                "2012",
+//                                                "2012",
 //                                                "2011",
 //                                                "2010",
 //                                                "2009",
 //                                                "2008",
-                                                "2007",
+//                                                "2007",
 //                                                "2006",
 //                                                "2005",
 //                                                "2004",
 //                                                "2003",
-                                                "2002"};
-
+//                                                "2002"
+    };
 
 
     public static void main(String[] args) {
@@ -48,12 +52,12 @@ public class ProcessTickers {
             List<TickerInfoDTO> allTickers = ExcelReader.getTickers(readDirectory);
 
             //Create portfolio map
-            Map<PortfolioGroupNamesEnum, List<TickerInfoDTO>> portfolioMap = new HashMap<>();
+            SortedMap<PortfolioGroupNamesEnum, List<TickerInfoDTO>> portfolioMap = new TreeMap<>();
 
-            System.out.println("Sorting tickers by size");
+            System.out.println("## Sorting all tickers by size");
             allTickers.sort(Comparator.comparingDouble(TickerInfoDTO::getSize));
 
-            generateTickersSpreedsheets(allTickers, portfolioMap);
+            generateTickersSpreedsheetsPortfolioA(allTickers, portfolioMap);
 
             try {
                 ExcelWriter.writeExcel(portfolioMap, writeDirectory);
@@ -63,80 +67,104 @@ public class ProcessTickers {
         }
     }
 
-    private static void generateTickersSpreedsheets(List<TickerInfoDTO> allTickers, Map<PortfolioGroupNamesEnum, List<TickerInfoDTO>> portfolioMap) {
+    private static void generateTickersSpreedsheetsPortfolioA(List<TickerInfoDTO> allTickers, Map<PortfolioGroupNamesEnum, List<TickerInfoDTO>> portfolioMap) {
         int tickersTotalSize = allTickers.size();
         System.out.println("Total Size: " + tickersTotalSize);
 
-        int sizeS = Math.round(tickersTotalSize / 2);
 
-        System.out.println("### Creating S");
+        System.out.println("#### Creating S");
+
+        int sizeS = Math.round(tickersTotalSize / 2);
 
         List<TickerInfoDTO> listS = new ArrayList<>(allTickers.stream().limit(sizeS).collect(Collectors.toList()));
         portfolioMap.put(S, listS);
 
         System.out.println("S Size: " + listS.size());
 
-        System.out.println("Sorting S by BMME and discarding null values");
-        List<TickerInfoDTO> listSBME = listS.stream()
-                .filter(e -> e.getBmme() != null)
-                .sorted(Comparator.comparingDouble(TickerInfoDTO::getBmme))
-                .collect(Collectors.toList());
+        List<TickerInfoDTO> listSBM = sortTickersByTypeWithoutNull(listS, S, BMME);
+        PortfolioGroupNamesEnum[] S_BMME_SUBGROUPS = {S_L3,S_NBM4,S_H3};
+        getSubgroupsLists(portfolioMap, listSBM, S_BMME_SUBGROUPS);
 
-        int sizeSBME = listSBME.size();
-        System.out.println("S Size without null BMME values: " + sizeSBME);
+        List<TickerInfoDTO> listSOP = sortTickersByTypeWithoutNull(listS, S, OP);
+        PortfolioGroupNamesEnum[] S_OP_SUBGROUPS = {S_W3,S_NOP4,S_R3};
+        getSubgroupsLists(portfolioMap, listSOP, S_OP_SUBGROUPS);
 
-        System.out.println("Creating S_L3 (30% -)");
-        int percentSL3 = Math.round(30 * sizeSBME / 100);
-        List<TickerInfoDTO> listSL3 = new ArrayList<>(listSBME.stream().limit(percentSL3).collect(Collectors.toList()));
-        portfolioMap.put(S_L3, listSL3);
-        System.out.println("S_L3 size: " + listSL3.size());
+        List<TickerInfoDTO> listSINV = sortTickersByTypeWithoutNull(listS, S, INV);
+        PortfolioGroupNamesEnum[] S_INV_SUBGROUPS = {S_C3,S_NINV4,S_A3};
+        getSubgroupsLists(portfolioMap, listSINV, S_INV_SUBGROUPS);
 
-        System.out.println("Creating S_NBM4 (40% +-)");
-        int percentSNBM4 = Math.round(40 * sizeSBME / 100);
-        List<TickerInfoDTO> listSNBM4 = new ArrayList<>(listSBME.stream().skip(percentSL3).limit(percentSNBM4).collect(Collectors.toList()));
-        portfolioMap.put(S_NBM4, listSNBM4);
-        System.out.println("S_NBM4 size: " + listSNBM4.size());
 
-        System.out.println("Creating S_H3 (30% +)");
-        int percentSH3 = (percentSL3 + percentSNBM4);
-        List<TickerInfoDTO> listSH3 = new ArrayList<>(listSBME.stream().skip(percentSH3).collect(Collectors.toList()));
-        portfolioMap.put(S_H3, listSH3);
-        System.out.println("S_H3 size: " + listSH3.size());
-
+        System.out.println("#### Creating B");
 
 //        long sizeB = Math.round(Math.floor((double)tickersTotalSize/2));
-//
-//        System.out.println("### Creating B");
-//        List<TickerInfoDTO> listB = new ArrayList<>(allTickers.stream().limit(sizeB).collect(Collectors.toList()));
-//        portfolioMap.put(B, listB);
-//
-//        System.out.println("S Size: " + listB.size());
-//
-//        System.out.println("Sorting S by BMME and discarding null values");
-//        listB = listB.stream()
-//                .filter(e -> e.getBmme() != null)
-//                .sorted(Comparator.comparingDouble(TickerInfoDTO::getBmme))
-//                .collect(Collectors.toList());
-//
-//        System.out.println("S Size without null BMME values: " + listB.size());
-//
-//        System.out.println("Creating S_L3 (30% -)");
-//        int percentSL3 = Math.round(30 * sizeB / 100);
-//        List<TickerInfoDTO> listBL3 = new ArrayList<>(listB.stream().limit(percentSL3).collect(Collectors.toList()));
-//        portfolioMap.put(S_L3, listBL3);
-//        System.out.println("S_L3 size: " + listBL3.size());
-//
-//        System.out.println("Creating S_NBM4 (40% +-)");
-//        int percentSNBM4 = Math.round(40 * sizeB / 100);
-//        List<TickerInfoDTO> listBNBM4 = new ArrayList<>(listB.stream().skip(percentSL3).limit(percentSNBM4).collect(Collectors.toList()));
-//        portfolioMap.put(S_NBM4, listBNBM4);
-//        System.out.println("S_NBM4 size: " + listBNBM4.size());
-//
-//        System.out.println("Creating S_H3 (30% +)");
-//        int percentSH3 = (percentSL3 + percentSNBM4);
-//        List<TickerInfoDTO> listBH3 = new ArrayList<>(listB.stream().skip(percentSH3).collect(Collectors.toList()));
-//        portfolioMap.put(S_H3, listBH3);
-//        System.out.println("S_H3 size: " + listBH3.size());
+
+        List<TickerInfoDTO> listB = new ArrayList<>(allTickers.stream().skip(sizeS).collect(Collectors.toList()));
+        portfolioMap.put(B, listB);
+
+        System.out.println("B Size: " + listB.size());
+
+        List<TickerInfoDTO> listBBM = sortTickersByTypeWithoutNull(listB, B, BMME);
+        PortfolioGroupNamesEnum[] B_BMME_SUBGROUPS = {B_L3,B_NBM4,B_H3};
+        getSubgroupsLists(portfolioMap, listBBM, B_BMME_SUBGROUPS);
+
+        List<TickerInfoDTO> listBOP = sortTickersByTypeWithoutNull(listB, B, OP);
+        PortfolioGroupNamesEnum[] B_OP_SUBGROUPS = {B_W3,B_NOP4,B_R3};
+        getSubgroupsLists(portfolioMap, listBOP, B_OP_SUBGROUPS);
+
+        List<TickerInfoDTO> listBINV = sortTickersByTypeWithoutNull(listB, B, INV);
+        PortfolioGroupNamesEnum[] B_INV_SUBGROUPS = {B_C3,B_NINV4,B_A3};
+        getSubgroupsLists(portfolioMap, listBINV, B_INV_SUBGROUPS);
+
+    }
+
+    private static void getSubgroupsLists(Map<PortfolioGroupNamesEnum, List<TickerInfoDTO>> portfolioMap, List<TickerInfoDTO> groupList, PortfolioGroupNamesEnum[] subgroupNames) {
+
+        System.out.println("## Creating " + subgroupNames[0] + " (30% -)");
+        int percentThirtyMinor = Math.round(30 * groupList.size() / 100);
+        List<TickerInfoDTO> listThirtyMinor = new ArrayList<>(groupList.stream().limit(percentThirtyMinor).collect(Collectors.toList()));
+        portfolioMap.put(subgroupNames[0], listThirtyMinor);
+        System.out.println(subgroupNames[0].name() + " size: " + listThirtyMinor.size());
+
+        System.out.println("## Creating " + subgroupNames[1] + " (40% +-)");
+        int percentFortyMedian = Math.round(40 * groupList.size() / 100);
+        List<TickerInfoDTO> listFortyMedian = new ArrayList<>(groupList.stream().skip(percentThirtyMinor).limit(percentFortyMedian).collect(Collectors.toList()));
+        portfolioMap.put(subgroupNames[1], listFortyMedian);
+        System.out.println(subgroupNames[1].name() + " size: " + listFortyMedian.size());
+
+        System.out.println("## Creating " + subgroupNames[2] + " (30% +)");
+        int percentThirtyBigger = (percentThirtyMinor + percentFortyMedian);
+        List<TickerInfoDTO> listThirtyBigger = new ArrayList<>(groupList.stream().skip(percentThirtyBigger).collect(Collectors.toList()));
+        portfolioMap.put(subgroupNames[2], listThirtyBigger);
+        System.out.println(subgroupNames[2].name() + " size: " + listThirtyBigger.size());
+    }
+
+    private static List<TickerInfoDTO> sortTickersByTypeWithoutNull(List<TickerInfoDTO> subgroupTickersList, PortfolioGroupNamesEnum listGroupName, SortingTypeEnum sortingType) {
+        System.out.println("## Sorting " + listGroupName.name() + " by " + sortingType.name() + " and discarding null values");
+
+        List<TickerInfoDTO> sortedListWithouNullValues;
+
+        if(BMME.equals(sortingType)){
+            sortedListWithouNullValues = subgroupTickersList.stream()
+                    .filter(e -> e.getBmme() != null)
+                    .sorted(Comparator.comparingDouble(TickerInfoDTO::getBmme))
+                    .collect(Collectors.toList());
+        } else if(OP.equals(sortingType)){
+            sortedListWithouNullValues = subgroupTickersList.stream()
+                    .filter(e -> e.getOp() != null)
+                    .sorted(Comparator.comparingDouble(TickerInfoDTO::getOp))
+                    .collect(Collectors.toList());
+        } else if (INV.equals(sortingType)){
+            sortedListWithouNullValues = subgroupTickersList.stream()
+                    .filter(e -> e.getInv() != null)
+                    .sorted(Comparator.comparingDouble(TickerInfoDTO::getInv))
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
+
+        System.out.println(listGroupName.name() + " Size without null " + sortingType.name() + " values: " + sortedListWithouNullValues.size());
+
+        return sortedListWithouNullValues;
     }
 
 }
---
+++ src/main/java/enums/SortingTypeEnum.java
@@ -0,0 +1,10 @@
+package enums;
+
+/**
+ * Created by lucas on 07/11/2017.
+ */
+public enum SortingTypeEnum {
+    BMME,
+    OP,
+    INV;
+}
